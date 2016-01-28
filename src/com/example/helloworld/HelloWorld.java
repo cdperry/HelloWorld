4265b1235e3fcfd54471e000d2dabb021b802829
Chris Perry <cdperry@gmail.com>
2016-01-27 19:52:20 -0600

    First commit from IntelliJ

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+/**
+ *  Hi.  Here is my header.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("It's me, Wombat!");
+    }
+
+}
